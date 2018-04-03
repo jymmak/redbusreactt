5ced2c3a59dc7a1bae6c2d1ec925531d98b1b943
Jymma Karina Mogollon Cardoza <henryfrancisco_fernando@hotmail.com>
2018-04-03 06:55:56 -0500

    cambios header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,11 @@ const Header = () => (
   <header>
     <div className="container">
       <div className="row justify-content-between">
-        <div className="col-4">
+        <div className="col-md-4">
           <img className='logo1' src={logo1} alt='logo1'></img>
           <img className='logo2' src={logo2} alt='logo2'></img>
         </div>
-        <div className="col-4">
+        <div className="col-md-3">
           <div class="dropdown">
             <button class="btn btn-danger dropdown-toggle language-button" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Idioma</button>
